96d21616679a21e94e8a77f9b0818a1c86e715db
martin islas <mislas87@gmail.com>
2015-09-18 14:13:02 -0500

    Change for public visibility to the classes

--- src/main/java/com/mcac0006/siftscience/result/domain/Access.java
+++ src/main/java/com/mcac0006/siftscience/result/domain/Access.java
@@ -1,13 +1,6 @@
-/*
- * COPYRIGHT © 2012-2013. OPENPAY.
- * PATENT PENDING. ALL RIGHTS RESERVED.
- * OPENPAY & OPENCARD IS A REGISTERED TRADEMARK OF OPENCARD INC.
- *
- * This software is confidential and proprietary information of OPENCARD INC.
- * You shall not disclose such Confidential Information and shall use it only
- * in accordance with the company policy.
- */
 package com.mcac0006.siftscience.result.domain;
+
+import java.util.List;
 
 import lombok.Getter;
 import lombok.Setter;
@@ -16,31 +9,22 @@ import lombok.ToString;
 import org.codehaus.jackson.annotate.JsonProperty;
 
 import com.fasterxml.jackson.annotation.JsonAutoDetect;
-import com.fasterxml.jackson.annotation.JsonAutoDetect.Visibility;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonAutoDetect.Visibility;
 import com.fasterxml.jackson.databind.PropertyNamingStrategy.LowerCaseWithUnderscoresStrategy;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
 
-
-/**
- * @author Oswaldo Martinez
- *
- */
 @ToString
 @Getter
 @Setter
 @JsonIgnoreProperties(ignoreUnknown = true)
 @JsonAutoDetect(fieldVisibility = Visibility.ANY, getterVisibility = Visibility.NONE, setterVisibility = Visibility.NONE, isGetterVisibility = Visibility.NONE)
 @JsonNaming(LowerCaseWithUnderscoresStrategy.class)
-public class SiftMerchantResponse {
-    
-	@JsonProperty(value="account_id")
-    private String accountId;
-    
-	@JsonProperty(value="production")
-    private Access production;
-	
-	@JsonProperty(value="sandbox")
-    private Access sandbox;
+public class Access {
+	@JsonProperty(value="api_keys")
+    private List<KeyRecord> apiKeys;
+
+	@JsonProperty(value="beacon_keys")
+    private List<KeyRecord> beaconKeys;
 
 }
--- src/main/java/com/mcac0006/siftscience/result/domain/KeyRecord.java
+++ src/main/java/com/mcac0006/siftscience/result/domain/KeyRecord.java
@@ -1,12 +1,3 @@
-/*
- * COPYRIGHT © 2012-2013. OPENPAY.
- * PATENT PENDING. ALL RIGHTS RESERVED.
- * OPENPAY & OPENCARD IS A REGISTERED TRADEMARK OF OPENCARD INC.
- *
- * This software is confidential and proprietary information of OPENCARD INC.
- * You shall not disclose such Confidential Information and shall use it only
- * in accordance with the company policy.
- */
 package com.mcac0006.siftscience.result.domain;
 
 import lombok.Getter;
@@ -16,31 +7,24 @@ import lombok.ToString;
 import org.codehaus.jackson.annotate.JsonProperty;
 
 import com.fasterxml.jackson.annotation.JsonAutoDetect;
-import com.fasterxml.jackson.annotation.JsonAutoDetect.Visibility;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonAutoDetect.Visibility;
 import com.fasterxml.jackson.databind.PropertyNamingStrategy.LowerCaseWithUnderscoresStrategy;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
 
-
-/**
- * @author Oswaldo Martinez
- *
- */
 @ToString
 @Getter
 @Setter
 @JsonIgnoreProperties(ignoreUnknown = true)
 @JsonAutoDetect(fieldVisibility = Visibility.ANY, getterVisibility = Visibility.NONE, setterVisibility = Visibility.NONE, isGetterVisibility = Visibility.NONE)
 @JsonNaming(LowerCaseWithUnderscoresStrategy.class)
-public class SiftMerchantResponse {
-    
-	@JsonProperty(value="account_id")
-    private String accountId;
-    
-	@JsonProperty(value="production")
-    private Access production;
+public class KeyRecord {
+	@JsonProperty(value="id")
+    private String id;
 	
-	@JsonProperty(value="sandbox")
-    private Access sandbox;
-
+	@JsonProperty(value="state")
+    private String state;
+	
+	@JsonProperty(value="key")
+    private String key;
 }
